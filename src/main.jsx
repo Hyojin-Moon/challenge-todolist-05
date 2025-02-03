d3d933b34ecab8336b686e5492ff814ecea49a5b
Hyojin-Moon <moontough8@gmail.com>
2025-02-03 16:56:53 +0900

    ✨ Update: state 로직을 reducer로 변경하기
    react 공식문서 따라해보기

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./app/store";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// import { combineReducers } from "@reduxjs/toolkit";
-// import todos from "./todos";
-
-
-// const rootReducer = combineReducers({
-//   todos,
-// });
-
-// export default rootReducer;
